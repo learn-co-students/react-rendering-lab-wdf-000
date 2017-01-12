51571ee83d9ef1c320dc1f93e56fc61d720f9390
Wendy Xu <guangw.xu@gmail.com>
2017-01-11 23:17:17 -0500

    Done.

--- components/Rating.js
+++ components/Rating.js
@@ -2,6 +2,24 @@ const React = require('react');
 
 class Rating extends React.Component {
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.rating > this.props.rating) {
+      this.setState({
+        increasing: true
+      });
+    } else if(nextProps.rating < this.props.rating) {
+      this.setState({
+        increasing: false,
+        decreasing: true
+      });
+    } else {
+      this.setState({
+        increasing: false,
+        decreasing: false
+      });
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {
